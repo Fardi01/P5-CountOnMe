f34dc01fcb710dbca87066925614fe8651929058
Fardi Issihaka <fardi.issihaka@laposte.net>
2021-06-01 13:07:32 +0200

    Design is Responsive

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,50 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
-    
+    // MARK: - Propriétés
+    // Renvoie le texte affiché sur l'écran de la calculatrice
     var elements: [String] {
         return textView.text.split(separator: " ").map { "\($0)" }
     }
-    
-    // Error check computed variables
+    // Error check computed variables (Controle les erreurs des elements calculé)
     var expressionIsCorrect: Bool {
         return elements.last != "+" && elements.last != "-"
     }
-    
+    //
     var expressionHaveEnoughElement: Bool {
         return elements.count >= 3
     }
-    
+    //
     var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-"
     }
-    
+    //
     var expressionHaveResult: Bool {
         return textView.text.firstIndex(of: "=") != nil
     }
-    
     // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    
-    // View actions
+    // MARK: - actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
-        
         if expressionHaveResult {
             textView.text = ""
         }
-        
         textView.text.append(numberText)
     }
-    
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
         if canAddOperator {
             textView.text.append(" + ")
@@ -62,7 +57,6 @@ class ViewController: UIViewController {
             self.present(alertVC, animated: true, completion: nil)
         }
     }
-    
     @IBAction func tappedSubstractionButton(_ sender: UIButton) {
         if canAddOperator {
             textView.text.append(" - ")
@@ -72,42 +66,40 @@ class ViewController: UIViewController {
             self.present(alertVC, animated: true, completion: nil)
         }
     }
-
+    @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
+    }
+    @IBAction func tappedDivisionButton(_ sender: UIButton) {
+    }
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         guard expressionIsCorrect else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
         guard expressionHaveEnoughElement else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
         // Create local copy of operations
         var operationsToReduce = elements
-        
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 {
             let left = Int(operationsToReduce[0])!
             let operand = operationsToReduce[1]
             let right = Int(operationsToReduce[2])!
-            
             let result: Int
             switch operand {
             case "+": result = left + right
             case "-": result = left - right
             default: fatalError("Unknown operator !")
             }
-            
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
             operationsToReduce.insert("\(result)", at: 0)
         }
-        
         textView.text.append(" = \(operationsToReduce.first!)")
+    }
+    @IBAction func tappedCleanButton(_ sender: UIButton) {
     }
 
 }
-
---
+++ CountOnMe/Model/SimpleCalc.swift
@@ -0,0 +1,14 @@
+//
+//  Compute.swift
+//  CountOnMe
+//
+//  Created by fardi Clk on 31/05/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SimpleCalc {
+    // Mettre la logique des boutons operateurs
+}
